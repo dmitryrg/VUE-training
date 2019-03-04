ce7eea32c8ea186e2287e46ed518915d210e3518
Dmitry Roganov <roganov@pharmhub.ru>
2019-03-04 16:33:42 +0300

    second commit

---
+++ users.js
@@ -0,0 +1,32 @@
+const users = [
+    {
+        firstName: 'Igor',
+        lastName: 'Ivanov',
+        patronymic: 'Vitalievich',
+        avatar: './avatars/1.jpg'
+    },
+    {
+        firstName: 'Petr',
+        lastName: 'Valeev',
+        patronymic: 'Ivanovich',
+        avatar: ''
+    },
+    {
+        firstName: 'Sergey',
+        lastName: 'Duhov',
+        patronymic: 'Pavlovich',
+        avatar: './avatars/3.jpg'
+    },
+    {
+        firstName: 'Alexandra',
+        lastName: 'Blakeeva',
+        patronymic: 'Evgenievna',
+        avatar: './avatars/4.jpg'
+    },
+    {
+        firstName: 'Vladimir',
+        lastName: 'Kovalev',
+        patronymic: 'Sergeevich',
+        avatar: './avatars/5.jpg'
+    },
+];
